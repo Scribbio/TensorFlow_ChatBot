130883173d5313affb95624e015123bc885ae916
Scribbio <joeditrolio@outlook.com>
2018-08-03 15:14:48 +0200

    Inclusion of key words matching function, only processes Reddit comments that contain a particular ley word.

--- chatbot_database.py
+++ chatbot_database.py
@@ -6,17 +6,33 @@ import json
 from datetime import datetime
 import time
 
-timeframe = '2010-12'
+timeframe = '2018-05'
 sql_transaction = []
 start_row = 0
-cleanup = 1000
+cleanup = 100000
+
+keyword_counterNew = 0
+keyword_counterOld = 0
 
 connection = sqlite3.connect('{}.db'.format(timeframe))
 c = connection.cursor()
 
+#
+# 'how’s everything?', 'how are things?', 'how’s life?', 'how’s your day?',         'how’s your day going?',
+# 'good to see you', 'nice to see you', 'long time no see', 'it’s been a while',            'good morning',
+# 'good afternoon', 'good evening', 'it’s nice to meet you', 'pleased to meet you',
+#             'welcome', 'how have you been?', 'yo!', 'are you OK?', 'you alright?', 'alright mate?', 'howdy!', 'sup?',
+#             'whazzup?', 'whassup', 'what’s up', 'hiya!', ,
+
+keywords = ['Hey ', 'Hello ', 'Hi ' 'how’s it going?', 'how are you doing?', 'what’s up?', 'what’s new?', 'what’s going on?',
+            ' atmosphere', ' clear sky', ' climate', ' cold', ' cyclone', ' fog',
+            ' humidity', ' humid', ' precipitation', ' prevailing wind', 'rain ', ' rainfall', ' summer', ' winter', ' autumn', 'spring', 'seasonality',
+            'smog ', ' sunny', ' snow', ' snowflakes', ' snowflakes', ' snowstorm ', ' temperature', ' temperature range', ' warm front', ' warm sector', ' warmer', ' weather',
+            ' wind ', ' windy ']
 
 def create_table():
     c.execute(
+        #Name of table - parent reply
         "CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)")
 
 
@@ -75,10 +91,22 @@ def acceptable(data):
         return False
     elif data == '[removed]':
         return False
-    elif subreddit != "AskReddit":
-        return False
     else:
         return True
+
+
+
+def contains_keywords(body):
+    for w in keywords:
+        if w in body:
+            keyword_counterNew = keyword_counterNew + 1
+        else:
+            continue
+
+    if keyword_counterNew > 0:
+            return True
+    else:
+            return False
 
 
 def find_parent(pid):
@@ -114,8 +142,8 @@ if __name__ == '__main__':
     row_counter = 0
     paired_rows = 0
 
-    # with open('J:/chatdata/reddit_data/{}/RC_{}'.format(timeframe.split('-')[0],timeframe), buffering=1000) as f:
-    with open('C:/Users/JoeDi/Desktop/MSC/RC_{}'.format(timeframe, timeframe), buffering=1000) as f:
+    # with open('D:/Téléchargements/{}/RC_{}'.format(timeframe.split('-')[0],timeframe), buffering=1000) as f:
+    with open('D:/Téléchargements/RC_{}'.format(timeframe, timeframe), buffering=1000) as f:
         for row in f:
             # print(row)
             # time.sleep(555)
@@ -134,21 +162,23 @@ if __name__ == '__main__':
                     subreddit = row['subreddit']
                     parent_data = find_parent(parent_id)
 
+                    #This first block looks to see if an existing comment exist, and replaces it if the current row has a higher score
                     existing_comment_score = find_existing_score(parent_id)
-                    if existing_comment_score:
+                    if existing_comment_score: #false if no existing comment exists
                         if score > existing_comment_score:
                             if acceptable(body):
                                 sql_insert_replace_comment(comment_id, parent_id, parent_data, body, subreddit,
                                                            created_utc, score)
 
+                    #If no particular existing comment exists, we insert new comment
                     else:
-                        if acceptable(body):
-                            if parent_data:
-                                if score >= 2:
+                        if acceptable(body) and contains_keywords(body):
+                            if parent_data: #if comment has a parent that it is replying to
+                                #if score >= 2: #This is the comment score threshold - designed to filter only good comments - won't be used as we need to maximise our comment number
                                     sql_insert_has_parent(comment_id, parent_id, parent_data, body, subreddit,
                                                           created_utc, score)
                                     paired_rows += 1
-                            else:
+                            else: #comment is inserted with no parent
                                 sql_insert_no_parent(comment_id, parent_id, body, subreddit, created_utc, score)
                 except Exception as e:
                     print(str(e))
@@ -157,7 +187,7 @@ if __name__ == '__main__':
                 print('Total Rows Read: {}, Paired Rows: {}, Time: {}'.format(row_counter, paired_rows,
                                                                               str(datetime.now())))
 
-                if row_counter % cleanup == 0:
+            if row_counter % cleanup == 0:
                     print("Cleanin up!")
                     sql = "DELETE FROM parent_reply WHERE parent IS NULL"
                     c.execute(sql)
